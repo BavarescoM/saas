7841d74bb69deed36a8b28b1c06135d1dce26ee6
BavarescoM <mauric222838@br-atacadao.corp>
2020-05-08 20:49:36 -0300

    cont 24

--- frontend/src/components/Members/index.js
+++ frontend/src/components/Members/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import Select from "react-select";
+import api from "../../sevices/api";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import MembersActions from "../../store/ducks/members";
@@ -9,12 +11,26 @@ import Modal from "../Modal";
 import Button from "../../styles/components/Button";
 
 class Members extends Component {
-  componentDidMount() {
+  state = {
+    roles: [],
+  };
+
+  async componentDidMount() {
     const { getMembersRequest } = this.props;
     getMembersRequest();
+    const response = await api.get("roles");
+    this.setState({
+      roles: response.data,
+    });
   }
+
+  handleRolesChange = (id, roles) => {
+    const { updateMemberRequest } = this.props;
+    updateMemberRequest(id, roles);
+  };
   render() {
     const { closeMembersModal, members } = this.props;
+    const { roles } = this.state;
     return (
       <Modal>
         <h1>Membros</h1>
@@ -23,6 +39,14 @@ class Members extends Component {
             {members.data.map((member) => (
               <li key={member.id}>
                 <strong>{member.user.name}</strong>
+                <Select
+                  isMulti
+                  options={roles}
+                  value={member.roles}
+                  getOptionLabel={(role) => role.name}
+                  getOptionValue={(role) => role.id}
+                  onChange={(value) => this.handleRolesChange(member.id, value)}
+                />
               </li>
             ))}
           </MembersList>
